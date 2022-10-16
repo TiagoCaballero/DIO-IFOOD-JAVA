e32bffae239cafdbe532cc59d61bc4924da58f25
TiagoCaballero <tiago.fiuza.caballero@gmail.com>
2022-10-16 12:53:14 +0100

    Meu primeiro projeto Java

--- meu_primeiro_projeto_java/src/App.java
+++ meu_primeiro_projeto_java/src/App.java
@@ -1,5 +1,7 @@
 public class App {
+    private static final String BOAS_VINDAS_TURMA_DIO = "Boas vindas turma DIO.";
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas turma DIO.");
+        System.out.println(BOAS_VINDAS_TURMA_DIO);
     }
 }
